5085cc31ab353f1e133ce34d2221f61dee44e948
param <param.mohandass@gmail.com>
2012-07-27 08:32:20 -0400

    Initial changes

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.6'
+
+gem 'pg'
+
+group :development, :test do
+	gem 'rspec-rails', '2.10.0'
+	gem 'capybara', '1.1.2'
+end
+
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails',   '3.2.4'
+  gem 'coffee-rails', '3.2.2'
+  gem 'uglifier', '1.2.3'
+end
+
+gem 'jquery-rails', '2.0.0'
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '0a6c68f831341239b491f63fad8b4095ea8555174d6aa151c702002cb9dce796acc3743abd54f66e597a8dc3c185f99dd3d991da97e866338aead1324a9cf545'
